11c0bfec2008b01fae6bf1f1874b5d6ee9b047be
nasir-uddin-dev <nasiruddin3954@gmail.com>
2025-10-17 19:24:21 +0600

    login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:assignment_task_manager/widgets/background_screen.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -8,7 +9,6 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
   final _formkey = GlobalKey<FormState>();
   final _emailTEController = TextEditingController();
   final _passwordTEController = TextEditingController();
@@ -16,25 +16,41 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 100),
-            Text(
-              "Get Started With",
-              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20,),
-            TextFormField(
-              controller: _emailTEController,
-              textInputAction: TextInputAction.next,
-              decoration: InputDecoration(
-                hintText: 'Enter a email'
+      body: BackgroundScreen(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 150),
+              Text(
+                "Get Started With",
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
               ),
-            ),
-          ],
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _emailTEController,
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.email),
+                  hintText: 'Enter a email',
+                  labelText: 'Email',
+                ),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _passwordTEController,
+                textInputAction: TextInputAction.next,
+                obscureText: true,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.password),
+                  suffixIcon: Icon(Icons.remove_red_eye_rounded),
+                  hintText: 'Enter a password',
+                  labelText: 'Password',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
