72b8bf39fb626967a3136290127994c6e64b7dbd
nasir-uddin-dev <nasiruddin3954@gmail.com>
2025-10-17 22:00:47 +0600

    sign up screen

---
+++ lib/screens/sign_up_screen.dart
@@ -0,0 +1,109 @@
+import 'package:assignment_task_manager/widgets/background_screen.dart';
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final _formkey = GlobalKey<FormState>();
+  final _emailTEController = TextEditingController();
+  final _firstNameTEController = TextEditingController();
+  final _lastNameTEController = TextEditingController();
+  final _mobileTEController = TextEditingController();
+  final _passwordTEController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BackgroundScreen(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 80),
+                Text(
+                  'Join With Us',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _emailTEController,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    hintText: 'Enter a email',
+                    labelText: 'Email',
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _firstNameTEController,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    hintText: 'Enter a first name',
+                    labelText: 'First Name',
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _lastNameTEController,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    hintText: 'Enter a last name',
+                    labelText: 'Last Name',
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _mobileTEController,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                    hintText: 'Enter a phone number',
+                    labelText: 'Phone',
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _passwordTEController,
+                  decoration: InputDecoration(
+                    hintText: 'Enter a password',
+                    labelText: 'Password',
+                  ),
+                ),
+                SizedBox(height: 30),
+                FilledButton(
+                  onPressed: () {},
+                  child: Icon(Icons.arrow_forward_ios_outlined, size: 24),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Have an account?'),
+                    TextButton(
+                      onPressed: _onTapSignInButton,
+                      child: Text(
+                        'Sign In',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onTapSignInButton(){
+    Navigator.pop(context);
+  }
+}
