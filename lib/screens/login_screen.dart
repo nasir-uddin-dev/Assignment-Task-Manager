da601425512302baaf6af3ff71554efcc9134653
nasir-uddin-dev <nasiruddin3954@gmail.com>
2025-10-17 19:43:28 +0600

    login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -19,37 +19,73 @@ class _LoginScreenState extends State<LoginScreen> {
       body: BackgroundScreen(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 150),
-              Text(
-                "Get Started With",
-                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 20),
-              TextFormField(
-                controller: _emailTEController,
-                textInputAction: TextInputAction.next,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.email),
-                  hintText: 'Enter a email',
-                  labelText: 'Email',
+          child: Form(
+            key: _formkey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Get Started With",
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
                 ),
-              ),
-              SizedBox(height: 20),
-              TextFormField(
-                controller: _passwordTEController,
-                textInputAction: TextInputAction.next,
-                obscureText: true,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.password),
-                  suffixIcon: Icon(Icons.remove_red_eye_rounded),
-                  hintText: 'Enter a password',
-                  labelText: 'Password',
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _emailTEController,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.email),
+                    hintText: 'Enter a email',
+                    labelText: 'Email',
+                  ),
                 ),
-              ),
-            ],
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _passwordTEController,
+                  textInputAction: TextInputAction.next,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.password),
+                    suffixIcon: Icon(Icons.remove_red_eye_rounded),
+                    hintText: 'Enter a password',
+                    labelText: 'Password',
+                  ),
+                ),
+                SizedBox(height: 20),
+                Center(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Forgotten Password',
+                      style: TextStyle(color: Colors.black45),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: FilledButton(
+                    style: FilledButton.styleFrom(
+                      backgroundColor: Colors.deepPurpleAccent,
+                      fixedSize: Size.fromWidth(double.maxFinite),
+                    ),
+                    onPressed: () {},
+                    child: Text("Login", style: TextStyle(fontSize: 24)),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Have an account?'),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
