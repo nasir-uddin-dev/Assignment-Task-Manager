8d47dd3e30211422c38cd93c55cebcc19f71b68f
nasir-uddin-dev <nasiruddin3954@gmail.com>
2025-10-18 08:19:37 +0600

    urls

--- lib/utils/urls.dart
+++ lib/utils/urls.dart
@@ -1,4 +1,7 @@
-class Urls{
+class Urls {
   static const String _baseUrl = "http://35.73.30.144:2005/api/v1";
   static const String registrationUrl = "$_baseUrl/Registration";
+  static const String loginUrl = "$_baseUrl/Login";
+  static const String verifyEmailUrl =
+      "$_baseUrl/RecoverVerifyEmail/email@gmail.com";
 }
